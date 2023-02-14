e9af46b082c4527e5e2c29edff09818b90a5c805
Roshni-Sharma-WD <roshnisharma040302@gmail.com>
2023-02-14 22:15:26 +0530

    JS

---
+++ LECTURE/Lec5/style.js
@@ -0,0 +1,27 @@
+function display() {
+    let nm = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let pass = document.getElementById('password').value;
+    let gen = document.getElementById('gender').value;
+
+
+    console.log(`Welcome ${nm} your email id is ${email}`);
+
+
+    console.log(`Your password is : $ {Pass}`);
+
+
+    console.log(`Your gender is $ {gen}`);
+
+    // if (is NaN (nm)) {
+    //     return true;
+    // }
+    // else {
+    //     return false;
+    //     console.log(Entere name is numberic : not allowed here);
+    // }
+
+}
+
+// function : perform a particular TextTrackList
+
